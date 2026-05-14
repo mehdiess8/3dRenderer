c1b05be80ae065ba039c7ea9ba57c5dd58cc6f17
mehdiess8 <mehdi8.ess@gmail.com>
2026-05-14 18:41:30 -0400

    Implemented Viewer Class and initialization functions

---
+++ 3d.py
@@ -0,0 +1,71 @@
+class Viewer(object):
+
+    def __init__(self):
+        """Initialize the viewer."""
+        self.init_interface()
+        self.init_opengl()
+        self.init_scene()
+        self.init_interaction()
+        init_primitives()
+
+    def init_interface(self):
+        """initialize the window and register the render function"""
+        glutInit()
+        glutInitWindowSize(640, 480)
+        glutCreateWindow("3D Renderer")
+        glutDisplayMode(GLUT_SINGLE | GLUT_RGB)
+        glutDisplayFunc(self.render)
+
+    def init_opengl(self):
+        """initalize the opengl settings to render the scene"""
+        self.inverseModelView = np.identity(4)
+        self.modelView = np.identity(4)
+
+        glEnable(GL_CULL_FACE)
+        glCullFace(GL_BACK)
+        glEnable(GL_DEPTH_TEST)
+        glDepthFunc(GL_LESS)
+        
+        glEnable(GL_LIGHT0)
+        glLightfv(GL_LIGHT0, GL_POSITION, GLfloat_4(0, 0, 1, 0))
+        glLightfv(GL_LIGHT0, GL_SPOT_DIRECTION, GLfloat_3(0, 0, -1))
+
+        glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+        glEnable(GL_COLOR_MATERIAL)
+        glClearColor(0.4, 0.4, 0.4, 0.0)
+
+    def init_scene(self):
+        """initialize the scene object and initial scene"""
+        self.scene = Scene()
+        self.create_sample_scene()
+
+    def create_sample_scene(self):
+        cube_node = Cube()
+        cube_node.translate(2, 0, 2)
+        cube_node.color_index = 2
+        self.scene.add_node(cube_node)
+
+        sphere_node = Sphere()
+        sphere_node.translate(-2, 0, 2)
+        sphere_node.color_index = 3
+        self.scene.add_node(sphere_node)
+
+        hierarchical_node = SnowFigure()
+        hierarchical_node.translate(-2, 0, -2)
+        self.scene.add_node(hierarchical_node)
+
+    def init_interaction(self):
+        """initialize the user interaction and callbacks"""
+        self.interaction = Interaction()
+        self.interaction.register_callback('pick', self.pick)
+        self.interaction.register_callback('move', self.move)
+        self.interaction.register_callback('place', self.place)
+        self.interaction.register_callback('rotate_color', self.rotate_color)
+        self.interaction.register_callback('scale', self.scale)
+
+    def main_loop(self):
+        glutMainLoop()
+
+if __name__ == '__main__':
+    viewer = Viewer()
+    viewer.main_loop()
