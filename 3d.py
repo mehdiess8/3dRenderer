15fc56261e3ec85503526b6f9e4bc2309d4e51df
mehdiess8 <mehdi8.ess@gmail.com>
2026-05-14 18:58:50 -0400

    added Node and Primitve classes with Sphere and Cube sub-classes

--- 3d.py
+++ 3d.py
@@ -130,6 +130,57 @@ class Scene(object):
         for node in self.node_list:
             node.render()
 
+class Node(object):
+    """ Base class for scene elements """
+    def __init__(self):
+        self.color_index = random.randint(color.MIN_COLOR, color.MAX_COLOR)
+        self.aabb = AABB([0.0, 0.0, 0.0], [0.5, 0.5, 0.5])
+        self.translation_matrix = numpy.identity(4)
+        self.scaling_matrix = numpy.identity(4)
+        self.selected = False
+
+    def render(self):
+        """ renders the item to the screen """
+        glPushMatrix()
+        glMultMatrixf(numpy.transpose(self.translation_matrix))
+        glMultMatrixf(self.scaling_matrix)
+        cur_color = color.COLORS[self.color_index]
+        glColor3f(cur_color[0], cur_color[1], cur_color[2])
+        if self.selected:  # emit light if the node is selected
+            glMaterialfv(GL_FRONT, GL_EMISSION, [0.3, 0.3, 0.3])
+
+        self.render_self()
+
+        if self.selected:
+            glMaterialfv(GL_FRONT, GL_EMISSION, [0.0, 0.0, 0.0])
+        glPopMatrix()
+
+    def render_self(self):
+        raise NotImplementedError(
+            "The Abstract Node Class doesn't define 'render_self'")
+
+class Primitive(Node):
+    def __init__(self):
+        super(Primitive, self).__init__()
+        self.call_list = None
+
+    def render_self(self):
+        glCallList(self.call_list)
+
+
+class Sphere(Primitive):
+    """ Sphere primitive """
+    def __init__(self):
+        super(Sphere, self).__init__()
+        self.call_list = G_OBJ_SPHERE
+
+
+class Cube(Primitive):
+    """ Cube primitive """
+    def __init__(self):
+        super(Cube, self).__init__()
+        self.call_list = G_OBJ_CUBE
+
 if __name__ == '__main__':
     viewer = Viewer()
     viewer.main_loop()
